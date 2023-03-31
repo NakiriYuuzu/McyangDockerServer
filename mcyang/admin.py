5163880ba698873db8b21ad2e9d7aa567092f12b
yuuzu <s1082978@gm.pu.edu.tw>
2023-03-31 21:57:44 +0800

    fix admin name problem

--- mcyang/admin.py
+++ mcyang/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(McyangTeacher)
-admin.site.register(McyangStudent)
-admin.site.register(McyangCourse)
+
+class McyangTeacherAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in McyangTeacher._meta.fields]
+
+
+class McyangStudentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in McyangStudent._meta.fields]
+
+
+class McyangCourseAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in McyangCourse._meta.fields]
+
+
+class McyangCourseRecordAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in McyangCourseRecord._meta.fields]
+
+
+admin.site.register(McyangTeacher, McyangTeacherAdmin)
+admin.site.register(McyangStudent, McyangStudentAdmin)
+admin.site.register(McyangCourse, McyangCourseAdmin)
+admin.site.register(McyangCourseRecord, McyangCourseRecordAdmin)
